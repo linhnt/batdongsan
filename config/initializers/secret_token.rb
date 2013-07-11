65978c3357daa5054c43b080c98801007ad1a420
Your Name <you@example.com>
2013-07-11 17:19:35 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Batdongsan::Application.config.secret_token = '0f661a36ccb72b210fe8995cb7d7091815006570e6399aff483193f48b7074764ae7b5ad7a7571bf2a89e597af40ecdc3f9d86e4548e9d1a1ea3cfaf73e6ff03'
