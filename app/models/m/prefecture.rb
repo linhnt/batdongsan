fac360ef52cc00008c0dd315900eb40669499a9e
Your Name <you@example.com>
2013-08-15 16:40:31 +0700

    update

--- app/models/m/prefecture.rb
+++ app/models/m/prefecture.rb
@@ -3,7 +3,7 @@ class M::Prefecture < ActiveRecord::Base
   attr_accessible :name
 
   has_many :m_districts, class_name: M::District.name,
-    foreign_key: :m_prefecture_id, dependent: :restrict
+    foreign_key: :m_prefecture_id
     
   belongs_to :m_city, class_name: M::City.name
   
